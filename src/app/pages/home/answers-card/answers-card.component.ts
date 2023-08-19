1c5305f22a25558363b4840ad1ecfdf1dfc84222
YoussefJarray <youssefjarray8@gmail.com>
2023-08-19 23:53:21 +0100

    fixed cards in home page to animate on scroll

--- src/app/pages/home/answers-card/answers-card.component.ts
+++ src/app/pages/home/answers-card/answers-card.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-answers-card',
   templateUrl: './answers-card.component.html',
 })
-export class AnswersCardComponent implements OnInit {
+export class AnswersCardComponent {
   @Input() card: { title: string; description: string; img: string } = { title: '', description: '', img: '' };
-  safeSvg!: SafeHtml; // Initialized in ngOnInit
+  isVisible = false;
+  @Input() cardIndex: number = 0;
+  safeSvg: SafeHtml = '';
 
   constructor(private sanitizer: DomSanitizer) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const yOffset = window.pageYOffset;
+    const element = document.getElementById(`card-${this.cardIndex}`);
+
+    if (element) {
+      const elementTop = element.getBoundingClientRect().top + window.scrollY;
+      const cardTop = yOffset + window.innerHeight * 0.8;
+
+      this.isVisible = elementTop < cardTop;
+    }
+  }
 
   ngOnInit() {
     this.safeSvg = this.sanitizer.bypassSecurityTrustHtml(this.card.img);
--- src/app/pages/home/answers/answers.component.ts
+++ src/app/pages/home/answers/answers.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-answers',
   templateUrl: './answers.component.html',
 })
-export class AnswersComponent implements OnInit {
+export class AnswersComponent {
   cards = [
     {
       title:
@@ -39,15 +39,4 @@ export class AnswersComponent implements OnInit {
         '<svg class="stroke-main w-32" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" /></svg>'
     },
   ];
-  currentCardIndex = 0;
-
-  ngOnInit() {
-    this.animateCards();
-  }
-
-  animateCards() {
-    setInterval(() => {
-      this.currentCardIndex = (this.currentCardIndex + 1) % this.cards.length;
-    }, 3000); // Change the interval time as needed (3000ms = 3 seconds)
-  }
 }
